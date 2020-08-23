db0e5086027795f99d464df803d4ce1a35bd4d70
DemonsTeam <69651430+demonsteam@users.noreply.github.com>
2020-08-23 22:54:52 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+echo "  <<<<      Welcome to Dscan Installer    >>>>"
+sleep 1.0
+sudo chmod +x dscan
+sudo chmod +x dscan-update
+sudo cp dscan /usr/bin
+sudo cp dscan-update /usr/bin
+clear
+echo "  <<<< Installing Your Dscan tool >>>>"
+sleep 1.0
+echo "  <<<< Dscan Has been Installed Successfully >>>>"
+echo "    <<<<  dscan --help for more >>>>" 
+
+
